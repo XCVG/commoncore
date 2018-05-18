a1a8c2453f72d41c498e4db08c9d224cf9182c93
Chris Leclair <xcvgvcx@gmail.com>
2018-05-18 12:48:23 -0700

    initial commit

---
+++ Assets/CommonCore/CCBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CommonCore
+{
+    /*
+     * CommonCore Base class
+     * Initializes CommonCore components
+     */
+    public class CCBase
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void OnGameStart()
+        {
+            Debug.Log("Initializing CommonCore...");
+
+            HookSceneLoad();
+
+            //TODO initialize modules
+
+            Debug.Log("...done!");
+        }
+
+        private static void HookSceneLoad()
+        {            
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            Debug.Log("Hooked scene load!");
+        }
+
+        static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Debug.Log("OnSceneLoaded: " + scene.name);
+            Debug.Log(mode);
+        }
+    }
+}
