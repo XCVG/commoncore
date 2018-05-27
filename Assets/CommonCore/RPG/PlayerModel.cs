d209167e224b42ef934ea4e7d10791e02742f387
Chris Leclair <xcvgvcx@gmail.com>
2018-05-27 15:59:08 -0700

    starting to build out RPG datastructures (threw out what was in Californium and started from scratch)

--- Assets/CommonCore/RPG/PlayerModel.cs
+++ Assets/CommonCore/RPG/PlayerModel.cs
@@ -8,11 +8,37 @@ namespace CommonCore.Rpg
     //temporary blank PlayerModel, will be completely rewritten anyway
     public class PlayerModel
     {
-       
+        public float Health
+        {
+            get
+            {
+                return DerivedStats.MaxHealth * HealthFraction;
+            }
+            set
+            {
+                HealthFraction = Health / DerivedStats.MaxHealth;
+            }
+        }
+
+        public float HealthFraction { get; set; }
+
+        public StatsSet BaseStats { get; private set; }
+        public StatsSet DerivedStats { get; private set; }
+
+        public List<Condition> Conditions { get; private set; }
 
         public PlayerModel()
         {
-            
+            Conditions = new List<Condition>();
+
+            //create blank stats and derive stats
+            BaseStats = new StatsSet(); //TODO load defaults
+            UpdateStats();
+        }
+
+        public void UpdateStats()
+        {
+            //TODO update stats
         }
 
         
---
+++ Assets/CommonCore/RPG/RpgMiscTypes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonCore.Rpg
+{
+    public enum DamageType
+    {
+        Normal, Impact, Explosive, Energy, Poison, Radiation
+    }
+
+    //kinda game dependent
+    public enum StatType
+    {
+        Resilience, Dexterity, Erudition, Intuition, Dialectic, Subterfuge, Serendipity
+    }
+
+    //mostly game dependent
+    //will handle variant trees in leveling logic rather than here
+    public enum SkillType
+    {
+        Melee, MeleeAlacrity, MeleePrecision, MeleeBrawn,
+        Archery, ArcheryDraw, ArcherySteady,
+        Guns, GunsAccuracy, GunsRapidity,
+        Athletics, AthleticsFurtive, AthleticsFleet,
+        Magic, MagicForce, MagicElemental, MagicDark,
+        Social, SocialExchange, SocialLeverage,
+        Security, SecurityMechanisms, SecurityComputers
+    }
+
+    //character model will have two of these: base and derived
+    public class StatsSet
+    {
+        public float MaxHealth { get; set; }
+        public float[] DamageResistance { get; set; }
+        public float[] DamageThreshold { get; set; }
+        
+        public int[] Stats { get; set; }
+        public int[] Skills { get; set; }
+
+        public StatsSet()
+        {
+            DamageResistance = new float[Enum.GetNames(typeof(DamageType)).Length];
+            DamageThreshold = new float[Enum.GetNames(typeof(DamageType)).Length];
+
+            Stats = new int[Enum.GetNames(typeof(StatType)).Length];
+            Skills = new int[Enum.GetNames(typeof(SkillType)).Length];
+        }
+        
+    }
+
+    //base class for permanent and temporary status conditions
+    public abstract class Condition
+    {
+
+    }
+}
