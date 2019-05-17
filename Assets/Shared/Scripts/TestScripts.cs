1157ca9f121a28c7027680eb3e743d9ed4a2b210
XCVGVCX <xcvgvcx@gmail.com>
2019-05-16 21:13:13 -0700

    added returning test script

---
+++ Assets/Shared/Scripts/TestScripts.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommonCore.Scripting;
+
+public static class TestScripts
+{
+
+    [CCScript]
+    public static bool LogAndReturnTrue(string arg)
+    {
+        Debug.Log("LogAndReturnTrue received arg " + arg);
+        return true;
+    }
+}
