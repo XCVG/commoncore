979d6bed229ce0c0dafdacd01cd6dfb6162e323e
Chris Leclair <xcvgvcx@gmail.com>
2021-09-24 15:23:14 -0700

    moved SlideshowControllerEx to correct namespace

--- Assets/Shared/Scripts/Experimental/SlideshowControllerEx.cs
+++ Assets/Shared/Scripts/Experimental/SlideshowControllerEx.cs
@@ -4,131 +4,135 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SlideshowControllerEx : MonoBehaviour
+namespace CommonCore.Experimental
 {
-    private Canvas RootCanvas = null;
-    private AspectRatioFitter AspectRatioFitter = null;
-    private Image BackgroundImage = null;
-    private Image SlideshowImage = null;
 
-    private static SlideshowControllerEx _Instance;
-
-    public static SlideshowControllerEx GetInstance()
+    public class SlideshowControllerEx : MonoBehaviour
     {
-        if(_Instance == null)
+        private Canvas RootCanvas = null;
+        private AspectRatioFitter AspectRatioFitter = null;
+        private Image BackgroundImage = null;
+        private Image SlideshowImage = null;
+
+        private static SlideshowControllerEx _Instance;
+
+        public static SlideshowControllerEx GetInstance()
         {
-            var go = new GameObject("SlideshowExperiment");
-            go.transform.SetParent(CoreUtils.GetUIRoot());
-            _Instance = go.AddComponent<SlideshowControllerEx>();
-            _Instance.Initialize();
+            if (_Instance == null)
+            {
+                var go = new GameObject("SlideshowExperiment");
+                go.transform.SetParent(CoreUtils.GetUIRoot());
+                _Instance = go.AddComponent<SlideshowControllerEx>();
+                _Instance.Initialize();
+            }
+
+            return _Instance;
         }
 
-        return _Instance;
-    }
-
-    public static void ClearInstance()
-    {
-        if(_Instance != null)
+        public static void ClearInstance()
         {
-            Destroy(_Instance.gameObject);
-            _Instance = null;
-        }
-    }
-
-    public bool HideHud
-    {
-        get
-        {
-            return RootCanvas.sortingOrder < 100;
-        }
-        set
-        {
-            RootCanvas.sortingOrder = value ? 110 : 1;
-        }
-    }
-
-    public bool UseBackground
-    {
-        get
-        {
-            return BackgroundImage.gameObject.activeSelf;
-        }
-        set
-        {
-            BackgroundImage.gameObject.SetActive(value);
-        }
-    }
-
-    public void ShowImage(string imageName)
-    {
-        if (string.IsNullOrEmpty(imageName))
-        {
-            Debug.LogWarning("Please use the ClearImage method instead of passing null/empty to ShowImage");
-            ClearImage();
-            return;
+            if (_Instance != null)
+            {
+                Destroy(_Instance.gameObject);
+                _Instance = null;
+            }
         }
 
-        var sprite = CoreUtils.LoadResource<Sprite>("Sequences/" + imageName);
-        if (sprite == null)
-            Debug.LogError($"Failed to load sprite \"Sequences/{imageName}\"");
+        public bool HideHud
+        {
+            get
+            {
+                return RootCanvas.sortingOrder < 100;
+            }
+            set
+            {
+                RootCanvas.sortingOrder = value ? 110 : 1;
+            }
+        }
 
-        AspectRatioFitter.aspectRatio = sprite.bounds.size.x / sprite.bounds.size.y;
-        SlideshowImage.sprite = sprite;
-        SlideshowImage.color = Color.white;
-    }
+        public bool UseBackground
+        {
+            get
+            {
+                return BackgroundImage.gameObject.activeSelf;
+            }
+            set
+            {
+                BackgroundImage.gameObject.SetActive(value);
+            }
+        }
 
-    public void ClearImage()
-    {
-        SlideshowImage.sprite = null;
-        SlideshowImage.color = new Color(0, 0, 0, 0);
-    }
+        public void ShowImage(string imageName)
+        {
+            if (string.IsNullOrEmpty(imageName))
+            {
+                Debug.LogWarning("Please use the ClearImage method instead of passing null/empty to ShowImage");
+                ClearImage();
+                return;
+            }
 
-    private void Initialize()
-    {
-        RootCanvas = gameObject.AddComponent<Canvas>();
-        //these need to be called in this specific order and I do not know why
-        RootCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        RootCanvas.gameObject.layer = 5;
-        RootCanvas.sortingOrder = 1;            
+            var sprite = CoreUtils.LoadResource<Sprite>("Sequences/" + imageName);
+            if (sprite == null)
+                Debug.LogError($"Failed to load sprite \"Sequences/{imageName}\"");
 
-        var cs = gameObject.AddComponent<CanvasScaler>();
-        cs.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-        cs.referenceResolution = new Vector2(1920, 1080);
-        cs.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+            AspectRatioFitter.aspectRatio = sprite.bounds.size.x / sprite.bounds.size.y;
+            SlideshowImage.sprite = sprite;
+            SlideshowImage.color = Color.white;
+        }
 
-        var bgObj = new GameObject("Background", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(AspectRatioFitter));
-        bgObj.transform.SetParent(RootCanvas.transform);
+        public void ClearImage()
+        {
+            SlideshowImage.sprite = null;
+            SlideshowImage.color = new Color(0, 0, 0, 0);
+        }
 
-        var bgRT = (RectTransform)bgObj.transform;
-        bgRT.anchorMin = Vector2.zero;
-        bgRT.anchorMax = Vector2.one;
-        bgRT.pivot = new Vector2(0.5f, 0.5f);
+        private void Initialize()
+        {
+            RootCanvas = gameObject.AddComponent<Canvas>();
+            //these need to be called in this specific order and I do not know why
+            RootCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            RootCanvas.gameObject.layer = 5;
+            RootCanvas.sortingOrder = 1;
 
-        var bgArf = bgObj.GetComponent<AspectRatioFitter>(); //hacky way of doing things
-        bgArf.aspectMode = AspectRatioFitter.AspectMode.EnvelopeParent;
-        bgArf.aspectRatio = 16f / 9f;
+            var cs = gameObject.AddComponent<CanvasScaler>();
+            cs.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            cs.referenceResolution = new Vector2(1920, 1080);
+            cs.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
 
-        BackgroundImage = bgObj.GetComponent<Image>();
-        BackgroundImage.sprite = null;
-        BackgroundImage.color = Color.black;
+            var bgObj = new GameObject("Background", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(AspectRatioFitter));
+            bgObj.transform.SetParent(RootCanvas.transform);
 
-        bgObj.SetActive(false);
+            var bgRT = (RectTransform)bgObj.transform;
+            bgRT.anchorMin = Vector2.zero;
+            bgRT.anchorMax = Vector2.one;
+            bgRT.pivot = new Vector2(0.5f, 0.5f);
 
-        var imageObj = new GameObject("Image", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(AspectRatioFitter));
-        imageObj.transform.SetParent(RootCanvas.transform);
+            var bgArf = bgObj.GetComponent<AspectRatioFitter>(); //hacky way of doing things
+            bgArf.aspectMode = AspectRatioFitter.AspectMode.EnvelopeParent;
+            bgArf.aspectRatio = 16f / 9f;
 
-        AspectRatioFitter = imageObj.GetComponent<AspectRatioFitter>();
-        AspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
-        AspectRatioFitter.aspectRatio = 16f / 9f;
+            BackgroundImage = bgObj.GetComponent<Image>();
+            BackgroundImage.sprite = null;
+            BackgroundImage.color = Color.black;
 
-        var imageRT = (RectTransform)imageObj.transform;
-        imageRT.anchorMin = Vector2.zero;
-        imageRT.anchorMax = Vector2.one;
-        imageRT.pivot = new Vector2(0.5f, 0.5f);
+            bgObj.SetActive(false);
 
-        SlideshowImage = imageObj.GetComponent<Image>();
-        SlideshowImage.color = new Color(0, 0, 0, 0);
+            var imageObj = new GameObject("Image", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(AspectRatioFitter));
+            imageObj.transform.SetParent(RootCanvas.transform);
 
-        //Debug.Log(RootCanvas.transform.position);
+            AspectRatioFitter = imageObj.GetComponent<AspectRatioFitter>();
+            AspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+            AspectRatioFitter.aspectRatio = 16f / 9f;
+
+            var imageRT = (RectTransform)imageObj.transform;
+            imageRT.anchorMin = Vector2.zero;
+            imageRT.anchorMax = Vector2.one;
+            imageRT.pivot = new Vector2(0.5f, 0.5f);
+
+            SlideshowImage = imageObj.GetComponent<Image>();
+            SlideshowImage.color = new Color(0, 0, 0, 0);
+
+            //Debug.Log(RootCanvas.transform.position);
+        }
     }
 }
